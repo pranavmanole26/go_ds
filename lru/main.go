9b6aa62235c15c65e564a90b6e9a5a4577877170
Pranav Ashok Manole <pmanole@avaya.com>
2023-06-16 12:12:01 +0530

    Optimized solution for LRU

--- lru/main.go
+++ lru/main.go
@@ -3,44 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(getPageFaults([]int{7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 3}, 4))
+	fmt.Println(getPageFaults([]int{7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2}, 4)) // 6
+	fmt.Println(getPageFaults([]int{}, 4))                                      // 0
+	fmt.Println(getPageFaults([]int{7}, 4))                                     // 1
+	fmt.Println(getPageFaults([]int{1, 1, 1, 1, 1, 1, 1}, 4))                   // 1
+	fmt.Println(getPageFaults([]int{1, 2, 3, 4, 5, 1, 1}, 4))                   // 6
 }
 
 func getPageFaults(pages []int, capacity int) int {
-	set := make(map[int]struct{})
-	uses := make(map[int]int)
+	set := make(map[int]struct{ index int })
 	pageFault := 0
 
 	for i := 0; i < len(pages); i++ {
 		if len(set) < capacity {
 			// If set is not full
 			if _, ok := set[pages[i]]; !ok {
-				set[pages[i]] = struct{}{}
 				pageFault++
 			}
-			uses[pages[i]] = i
 		} else {
 			// If set is full
 			if _, ok := set[pages[i]]; !ok {
-				set[pages[i]] = struct{}{}
 				pageFault++
 
-				lruI, lru := i-0, uses[i-0]
+				// LRU algo start
+				lruI, lru := i-0, 0
 
-				for k, v := range uses {
-					if v < lruI {
-						lruI = v
+				for k, v := range set {
+					if v.index < lruI {
+						lruI = v.index
 						lru = k
 					}
 				}
-
 				delete(set, lru)
-				delete(uses, lru)
-				set[pages[i]] = struct{}{}
+				// LRU algo End
 			}
-			uses[pages[i]] = i
-
 		}
+		set[pages[i]] = struct{ index int }{i}
 	}
 
 	return pageFault
