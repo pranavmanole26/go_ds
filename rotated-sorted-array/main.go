4781fe152bd75695ff14dae11d1736cc7a32f1b1
Pranav Ashok Manole <pmanole@avaya.com>
2023-08-29 23:12:59 +0530

    added comment

--- rotated-sorted-array/main.go
+++ rotated-sorted-array/main.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{7, 8, 9, 1, 2, 3, 4, 5, 6}
+	// nums := []int{7, 8, 9, 1, 2, 3, 4, 5, 6}
 
-	fmt.Println(findElement(nums, 10))
+	// fmt.Println(findElement(nums, 10))
+
+	// nums1 := []int{3, 1, 2, 3, 3}
+	nums1 := []int{7, 8, 9, 1, 2, 3, 4, 5, 6}
+	fmt.Println(hasElementFromDuplicateElements(nums1, 10))
 }
 
+// For non duplicate elements
 func findElement(nums []int, num int) int {
 	lI := 0
 	rI := len(nums) - 1
@@ -36,6 +41,42 @@ func findElement(nums []int, num int) int {
 		} else {
 			// Right array is sorted
 
+			if nums[lI] <= num && num <= nums[mI] {
+				rI = mI - 1
+			} else {
+				lI = mI + 1
+			}
+		}
+	}
+}
+
+func hasElementFromDuplicateElements(nums []int, num int) bool {
+	lI := 0
+	rI := len(nums) - 1
+
+	for {
+		if lI > rI {
+			return false
+		}
+		mI := (lI + rI) / 2
+		if nums[mI] == num {
+			return true
+		}
+		// Check for this corner case where left, right and num could be the same values
+		// If this is the case then trim down the array
+		if nums[lI] == num && nums[rI] == num {
+			lI = lI + 1
+			rI = rI - 1
+			continue
+		}
+
+		if nums[mI] < nums[rI] {
+			if nums[mI] <= num && num <= nums[rI] {
+				lI = mI + 1
+			} else {
+				rI = mI - 1
+			}
+		} else {
 			if nums[lI] <= num && num <= nums[mI] {
 				rI = mI - 1
 			} else {
