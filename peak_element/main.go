4b3d025af80ff28b38ea526ba7bf3852bdf8672b
Pranav Ashok Manole <--help>
2023-09-21 23:33:48 +0530

    added find peak solution

---
+++ peak_element/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4, 3, 2, 1}
+	fmt.Println(getPeakElement(nums))
+}
+
+func getPeakElement(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	if nums[0] > nums[1] {
+		return nums[0]
+	}
+	n := len(nums)
+	if nums[n-1] > nums[n-2] {
+		return nums[n-1]
+	}
+
+	li := 1
+	ri := n - 2
+
+	for {
+		// if li and ri crosses each other then return either of them
+		if li > ri {
+			return nums[li]
+		}
+		// find mid index
+		mi := (li + ri) / 2
+		// check if mid index element is peak
+		if nums[mi] > nums[mi-1] && nums[mi] > nums[mi+1] {
+			return nums[mi]
+		}
+		// check if mid index element is greater than previous one
+		if nums[mi] >= nums[mi-1] {
+			// shift li, i.e. the numbers are growing towards right and the peak is at right
+			li = mi + 1
+		}
+		if nums[mi] >= nums[mi+1] {
+			// shift ri, i.e. the numbers are growing towards left and the peak is at left
+			ri = mi - 1
+		}
+	}
+}
---
+++ peak_element/main_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetPeakElement(t *testing.T) {
+	assertion := assert.New(t)
+
+	testCases := []struct {
+		name   string
+		input  []int
+		output int
+	}{
+		{
+			name:   "First element peak",
+			input:  []int{3, 2, 1},
+			output: 3,
+		},
+		{
+			name:   "Last element peak",
+			input:  []int{1, 2, 3},
+			output: 3,
+		},
+		{
+			name:   "Single element",
+			input:  []int{1},
+			output: 1,
+		},
+		{
+			name:   "Two elements 1",
+			input:  []int{2, 3},
+			output: 3,
+		},
+		{
+			name:   "Two elements 2",
+			input:  []int{3, 2},
+			output: 3,
+		},
+		{
+			name:   "Inside peak",
+			input:  []int{1, 2, 3, 4, 3, 2},
+			output: 4,
+		},
+		{
+			name:   "All peak",
+			input:  []int{3, 3, 3},
+			output: 3,
+		},
+		{
+			name:   "Duplicate consequitive peaks",
+			input:  []int{1, 2, 3, 3, 2},
+			output: 3,
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			actualOutput := getPeakElement(tC.input)
+			assertion.Equal(tC.output, actualOutput)
+		})
+	}
+
+}
+
+func BenchmarkGetPeakElement(b *testing.B) {
+	nums := []int{1, 2, 3, 4, 3, 2}
+	for i := 0; i < b.N; i++ {
+		getPeakElement(nums)
+	}
+}
