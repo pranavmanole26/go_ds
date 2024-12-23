c095fe061982b428f53f1425458454a4abe9450a
pranavmanole26 <pranavmanole26@gmail.com>
2024-12-23 23:40:36 +0530

    added few more easy examples

--- array_easy/main.go
+++ array_easy/main.go
@@ -72,6 +72,53 @@ func RemoveDuplicatesFromSorted(nums []int) []int {
 	return nums
 }
 
+func ShiftArrayElementsByKPlacesToLeft(nums []int, k int) []int {
+	temp := make([]int, k)
+	for i := 0; i < k; i++ {
+		temp[i] = nums[i]
+	}
+	temp1 := make([]int, len(nums)-k, len(nums))
+	for i := 0; i < len(nums)-k; i++ {
+		temp1[i] = nums[i+k]
+	}
+	temp1 = append(temp1, temp...)
+	return temp1
+}
+
+func ShiftAllOccurancesOfGivenNumberToTheLeft(nums []int, num int) []int {
+	numI := 0
+	firstNum := false
+	for i := 0; i < len(nums); i++ {
+		// This if must execute first in the iteration in order to decide the first occurance of "num" and store it's index in "numI"
+		// Once we get the index we should go for next iteration
+		if nums[i] == num && !firstNum {
+			numI = i
+			firstNum = true
+			continue
+		}
+		// Once we get the first occurance of the "num" we need to check if the next element is not the "num"
+		// If it is not "num" then swap these two numbers and increment numI by one
+		if nums[i] != num && firstNum {
+			nums[i], nums[numI] = nums[numI], nums[i]
+			numI++
+		}
+	}
+	return nums
+}
+
+func FindTheMissingNumberFromSeries(nums []int) int {
+	actualSum := 0
+	// Find actual sum
+	for _, num := range nums {
+		actualSum += num
+	}
+	// Find sum of ideal series
+	// Sn = n/2 * (2*a + (n-1)*d)
+	sumOfIdealSeries := (len(nums) + 1) / 2 * (2*nums[0] + (nums[len(nums)-1]-1)*1)
+	// The difference between ideal sum and actual sum is the missing number
+	return sumOfIdealSeries - actualSum
+}
+
 func main() {
 	// fmt.Println(LargestElement([]int{2, 4, 5, 3, 1}))
 	// fmt.Println(SecondSmallestAndSecondLargest([]int{2, 4, 5, 3, 1}))
@@ -83,5 +130,8 @@ func main() {
 	// i+1 -> 2
 	// nums[:i+1] -> 1,2
 	// nums[i+1:] -> 3
-	fmt.Println(RemoveDuplicatesFromSorted([]int{1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 6}))
+	// fmt.Println(RemoveDuplicatesFromSorted([]int{1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 6}))
+	// fmt.Println(ShiftArrayElementsByKPlacesToLeft([]int{1, 2, 3, 4, 5}, 2))
+	// fmt.Println(ShiftAllOccurancesOfGivenNumberToTheLeft([]int{1, 2, 3, 4, 2, 3, 2, 1, 2}, 3))
+	fmt.Println(FindTheMissingNumberFromSeries([]int{1, 2, 3, 5, 6}))
 }
