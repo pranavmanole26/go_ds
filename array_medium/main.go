a7e3fc12645b4dc85c550dd2988c7ad90e0801b5
pranavmanole26 <pranavmanole26@gmail.com>
2024-12-25 00:21:09 +0530

    Added few more medium examples

--- array_medium/main.go
+++ array_medium/main.go
@@ -66,6 +66,45 @@ func UnionOfTwoArrays(first, second []int) []int {
 	return final
 }
 
+// Question: https://takeuforward.org/arrays/find-the-number-that-appears-once-and-the-other-numbers-twice/
+func FindSingleElement(nums []int) int {
+	mapOfOccurance := map[int]struct{}{}
+	for _, n := range nums {
+		if _, ok := mapOfOccurance[n]; !ok {
+			mapOfOccurance[n] = struct{}{}
+		} else {
+			delete(mapOfOccurance, n)
+		}
+	}
+	for k := range mapOfOccurance {
+		return k
+	}
+	return -1
+}
+
+// Question: https://takeuforward.org/data-structure/longest-subarray-with-given-sum-k/
+// TODO: Understand the hashing solution
+func LongestSubArrayWithSum(nums []int, sum int) int {
+	longestSubArrayLength := 0
+	for i := 0; i < len(nums); i++ {
+		curSum := 0
+		for j := i; j < len(nums); j++ {
+			curSum += nums[j]
+			if curSum == sum && longestSubArrayLength < j-i+1 {
+				longestSubArrayLength = j - i + 1
+				break
+			}
+			if curSum > sum {
+				break
+			}
+		}
+	}
+	return longestSubArrayLength
+}
+
 func main() {
-	fmt.Println(UnionOfTwoArrays([]int{1, 2, 2, 4, 6}, []int{2, 3, 4, 4, 4, 5, 7, 8}))
+	// fmt.Println(UnionOfTwoArrays([]int{1, 2, 2, 4, 6}, []int{2, 3, 4, 4, 4, 5, 7, 8}))
+	// fmt.Println(FindSingleElement([]int{1, 1, 2, 3, 4, 3, 2}))
+	fmt.Println(LongestSubArrayWithSum([]int{2, 3, 5, 1, 9}, 10))
+	fmt.Println(LongestSubArrayWithSum([]int{-1, 1, 1}, 1))
 }
